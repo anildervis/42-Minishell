f0d4c0afca8c43c20213d3f14df1afff13d65dab
anilalis <anildervisogluib@gmail.com>
2023-03-23 02:56:17 +0300

    new definitions added

--- minishell.h
+++ minishell.h
@@ -7,22 +7,34 @@
 #include <fcntl.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <dirent.h>
+
+#define DOUBLE_QUOTE = "\""
+#define SINGLE_QUOTE = "\'"
+#define DOLLAR_SIGN = "$"
+#define WILD_CARD = "*"
+#define QUESTION_MARK = "?"
+#define BRACETS = "{}"
+#define SLASH = "/"
 
 typedef struct	s_ms
 {
+	int			pid;
+	int			error_status;
 	char		**ev;
 	char		**path;
 }				t_ms;
 
 enum tokens{
 	TOKEN_PIPE,
-	TOKEN_APH,
 	TOKEN_OR,
 	TOKEN_AND,
 	TOKEN_GREATER,
 	TOKEN_SMALLER,
 	TOKEN_APPEND,
 	TOKEN_HERE_DOC,
+	TOKEN_OPEN_PAR,
+	TOKEN_CLOSE_PAR,
 	TOKEN_STR
 }
 
@@ -40,12 +52,11 @@ char	*ft_strdup(char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	**ft_split(char const *s, char c);
+int		find_pair(char *input, char c);
 
 //--------------tokenizer--------------
-int is_token(char token);
 int add_token(char *input, t_token **command_table, enum tokens type, int len);
 t_token **find_token(char *input, t_token **command_table);
-t_token **expand_wildcard(t_token **command_table);
 t_token *tokenizer(char *input);
 
 #endif
