e844c300fa13fbafe00ee1a46918e6f0157773ef
anilalis <anildervisogluib@gmail.com>
2023-03-23 02:56:52 +0300

    pair finding function for quotes and bracets

---
+++ utils/find_pair.c
@@ -0,0 +1,19 @@
+/*
+verilen inputun ilk karakterinin aynısını 
+kaç karakter sonrasında bulduğunu döndürüyor
+*/
+
+int find_pair(char *input, char c)
+{
+    int i;
+    char *tmp;
+
+    i = 0;
+    tmp = input;
+    while (*(tmp + ++i))
+    {
+        if (*(tmp + i) == c)
+            return i;
+    }
+    return -1;
+}
