744145ebfc452f1b457dcaf8145a4d30c167a7ed
anilalis <anildervisogluib@gmail.com>
2023-03-27 22:33:29 +0300

    fixed structured, some norm checks

--- minishell.h
+++ minishell.h
@@ -62,27 +62,38 @@ typedef struct s_parsed
 	char		**arguments;
 	t_token		*paranthesis;
 	t_file		*file_list;
-	t_parsed	*prev;
-	t_parsed	*next;
+	struct s_parsed	*prev;
+	struct s_parsed	*next;
 }				t_parsed;
 
 //--------------lexer--------------
-int is_metacharacter(char c);
-int token_str_lexer(char *input);
-int add_token(char *input, t_token *command_table, enum tokens type, int len);
-void find_token(char *input, t_token *command_table);
-t_token *tokenizer(char *input);
+int			is_metacharacter(char c);
+int			token_str_lexer(char *input);
+int			add_token(char *input, t_token *command_table, enum tokens type, int len);
+void		find_token(char *input, t_token *command_table);
+t_token		*tokenizer(char *input);
 
 //--------------expander--------------
-int add_double_quote(char **str, char *val);
-int add_single_quote(char **str, char *val);
-int add_dollar(char **str, char *val);
-int add_char(char **str, char *val);
-char *check_str(char *value);
-int wild_path(char *wild_one, char *expected_one);
-void wildcard(char *path, char **destined_path, int way, t_token **command_table);
-void add_wildcard_to_list(char *path, t_token **command);
-t_token *expander(t_token *command_list);
+int			add_double_quote(char **str, char *val);
+int			add_single_quote(char **str, char *val);
+int			add_dollar(char **str, char *val);
+int			add_char(char **str, char *val);
+char		*check_str(char *value);
+int			wild_path(char *wild_one, char *expected_one);
+void		wildcard(char *path, char **destined_path, int way, t_token **command_table);
+void		add_wildcard_to_list(char *path, t_token **command);
+t_token		*expander(t_token *command_list);
+
+//--------------expander--------------
+int			andor_count(t_token *command_table);
+t_parsed	*new_parse_command(int in_file, int out_file);
+void		add_andor_list(enum tokens andor, t_parsed *command, t_parsed **andor_table);
+void		add_redirection(t_token **command_table, t_parsed **command);
+void		skip_paranthesis(t_token **command_table);
+t_parsed	*add_parse(t_token **command_table, t_parsed **old_command);
+void		add_argument(t_token **command_table, t_parsed **command);
+void		add_paranthesis(t_token **command_table, t_parsed **command);
+t_parsed	**parse_commands(int in_file, int out_file, t_token *command_table);
 
 
 #endif
