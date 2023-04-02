5a1d102334927ddfd165251f114d1ff85d1b5861
anilalis <anildervisogluib@gmail.com>
2023-04-02 19:49:16 +0300

    .

--- minishell.h
+++ minishell.h
@@ -130,7 +130,6 @@ void	free_array(char **arr);
 
 //builtin
 void	run_builtin(char **exe);
-int		is_builtin(char *command);
 void	builtin_cd(char **execute);
 void	builtin_echo(char **input);
 void	builtin_env(void);
