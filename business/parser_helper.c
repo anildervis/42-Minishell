af8a255921535d62fb17568200341b9f8a493248
Bilal Salih Nurtas <binurtas@k1m14s08.42istanbul.com.tr>
2023-05-16 17:22:21 +0300

    updated for norm --parser done

--- business/parser_helper.c
+++ business/parser_helper.c
@@ -26,3 +26,50 @@ int	ft_decide_type(t_token **command_table, t_parsed **command)
 	}
 	return (1);
 }
+
+void	add_andor_list(t_parsed *command, t_parsed **andor_table)
+{
+	int	i;
+
+	i = 0;
+	while (andor_table[i])
+		i++;
+	andor_table[i] = command;
+	andor_table[i + 1] = NULL;
+}
+
+void	skip_paranthesis(t_token **command_table)
+{
+	int	i;
+
+	i = 0;
+	while ((*command_table))
+	{
+		if ((*command_table)->type == TOKEN_OPEN_PAR)
+			i++;
+		else if ((*command_table)->type == TOKEN_CLOSE_PAR)
+			i--;
+		if (!i)
+			break ;
+		(*command_table) = (*command_table)->next;
+	}
+	// error;
+}
+
+int	andor_count(t_token *command_table)
+{
+	int		i;
+	t_token	*tmp_list;
+
+	i = 0;
+	tmp_list = command_table;
+	while (tmp_list)
+	{
+		if (tmp_list->type == TOKEN_AND || tmp_list->type == TOKEN_OR)
+			i++;
+		else if (tmp_list->type == TOKEN_OPEN_PAR)
+			skip_paranthesis(&tmp_list);
+		tmp_list = tmp_list->next;
+	}
+	return (i);
+}
