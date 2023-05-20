b1b7eb4ca0e3acc06eec4cf39db9f83428d819b6
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-05-20 15:27:25 +0300

    normed

--- utils/t_str_lexer.c
+++ utils/t_str_lexer.c
@@ -12,20 +12,20 @@
 
 #include "../minishell.h"
 
-int token_str_lexer(char *input)
+int	token_str_lexer(char *input)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (*(input + i) && is_metacharacter(*(input + i))
-        && ft_strnsearch((input + i), " \t\n", 1))
-    {
-        if (*(input + i) == *DOUBLE_QUOTE)
-            i += find_pair(input + i, *DOUBLE_QUOTE) + 1;
-        else if (*(input + i) == *SINGLE_QUOTE)
-            i += find_pair(input + i, *SINGLE_QUOTE) + 1;
-        else
-            i++;
-    }
-    return (i);
+	i = 0;
+	while (*(input + i) && is_metacharacter(*(input + i))
+		&& ft_strnsearch((input + i), " \t\n", 1))
+	{
+		if (*(input + i) == *DOUBLE_QUOTE)
+			i += find_pair(input + i, *DOUBLE_QUOTE) + 1;
+		else if (*(input + i) == *SINGLE_QUOTE)
+			i += find_pair(input + i, *SINGLE_QUOTE) + 1;
+		else
+			i++;
+	}
+	return (i);
 }
