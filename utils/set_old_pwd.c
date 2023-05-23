73f667abd01fc95ec3dd7535fa29266dccb425ad
Anıl Dervişoğlu <anildervisoglu@gmail.com>
2023-05-23 11:48:01 +0300

    pwd double free fixed

--- utils/set_old_pwd.c
+++ utils/set_old_pwd.c
@@ -8,7 +8,7 @@ void	set_old_pwd(char *pwd)
 	if (!command)
 		return ;
 	command[0] = ft_strdup("export");
-	command[1] = ft_strjoin_freed("OLDPWD=", pwd, 0b01);
+	command[1] = ft_strjoin("OLDPWD=", pwd);
 	command[2] = 0;
 	builtin_export(command);
 	free_array(command);
