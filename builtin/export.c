4c325391c6eaed8c5dc7bfabd092c25ad0fb9aa5
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-06-15 02:50:55 +0300

    comment lines - prints deleted

--- builtin/export.c
+++ builtin/export.c
@@ -18,67 +18,46 @@ void	new_builtin_export(char **input)
 	int check_export;
 	int i;
 
-	printf("0 buradayim\n");
 	if (!input[1])
 		ft_pure_export_print();
 	i = 1;
 	while (input[i])
 	{
-		printf("1 buradayim\n");
 		if (check_char(input[0], input[i]))
 		{
 			i++;
 			continue ;
 		}
-		printf("2 buradayim\n");
-		check_export = ft_is_exist_export(input[i]); // -1 -> yok else index döner
+		check_export = ft_is_exist_export(input[i]);
 		check_env = ft_is_exist_env(input[i]);
-		printf("\n\nenv_ciktisi = %d\n\n", check_env);
-		printf("\n\nexport_ciktisi = %d\n\n", check_export);
-		printf("3 buradayim\n");
 		if (is_there_equal(input[i]))
 		{
-			printf("4 buradayim\n");
 			if (check_export >= 0)
 			{
-				printf("4.1 buradayim\n");
 				free(g_ms.export[check_export]);
 				g_ms.export[check_export] = ft_strdup(input[i]);
 			}
 			else
-			{
-				printf("4.2 buradayim\n");
 				add_export(input[i]);
-			}
 			if (check_env >= 0)
 			{
-				printf("4.3 buradayim\n");
 				free(g_ms.ev[check_env]);
 				g_ms.ev[check_env] = ft_strdup(input[i]);
 			}
 			else
-			{
-				printf("4.4 buradayim\n");
 				add_env(input[i]);
-			}
 		}
 		else
 		{
-			printf("6 buradayim\n");
 			if (check_export >= 0)
 			{
-				printf("6.1 buradayim\n");
 				free(g_ms.export[check_export]);
 				g_ms.export[check_export] = ft_strdup(input[i]);
 			}
 			else
-			{
-				printf("6.2 buradayim\n");
 				if (check_export != -2)
 					add_export(input[i]);
-			}
 		}
-		printf("7 buradayim\n");
 		i++;
 	}
 }
